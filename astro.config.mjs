8abcba17f993848d5e15ec37f2975bcaa4af96c4
hellokaton <hellokaton@gmail.com>
2025-03-12 17:12:41 +0800

    refactor(fetch-github-users):移除用户数据缓存功能 - 删除了缓存文件的加载、更新和保存逻辑 - 移除了 markdownDevelopers 的引用 - 简化了用户数据的处理流程

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
+  // site: "http://localhost:4321",
+  site: "https://china-ranking.vercel.app",
   integrations: [tailwind()],
 });
